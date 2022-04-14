da90849064c0a9612f7487671fc4cd7200ec711a
Jerrad Thramer <Jerrad.Thramer@mlb.com>
2022-04-14 13:54:10 -0600

    Creating shell script that will generate fat binary for us to consume in app.

---
+++ generateFatBinary.sh
@@ -0,0 +1,29 @@
+!#/bin/sh
+
+rm -r build/*
+rm -rf Products/*.xcframework
+
+xcodebuild archive \
+-project SecretSquirrelFramework/SecretSquirrelFramework.xcodeproj \
+-scheme SecretSquirrelFramework \
+-configuration Release \
+-destination 'generic/platform=iOS Simulator' \
+-archivePath './build/SecretSquirrelFramework-iphonesimulator.xcarchive' \
+SKIP_INSTALL=NO \
+BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
+
+xcodebuild archive \
+-project SecretSquirrelFramework/SecretSquirrelFramework.xcodeproj \
+-scheme SecretSquirrelFramework \
+-configuration Release \
+-destination 'generic/platform=iOS' \
+-archivePath './build/SecretSquirrelFramework-iphoneos.xcarchive' \
+SKIP_INSTALL=NO \
+BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
+
+xcodebuild -create-xcframework \
+-archive  'build/SecretSquirrelFramework-iphonesimulator.xcarchive' \
+-framework 'SecretSquirrelFramework.framework' \
+-archive 'build/SecretSquirrelFramework-iphoneos.xcarchive' \
+-framework 'SecretSquirrelFramework.framework' \
+-output 'Products/SecretSquirrel.xcframework'
