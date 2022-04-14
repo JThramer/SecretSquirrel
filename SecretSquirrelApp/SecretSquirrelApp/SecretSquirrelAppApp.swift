fef99c0ce613a81878d67ca4fa669f08309d1efb
Jerrad Thramer <Jerrad.Thramer@mlb.com>
2022-04-14 13:03:36 -0600

    Creating SecretSquirrelApp demo

---
+++ SecretSquirrelApp/SecretSquirrelApp/SecretSquirrelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecretSquirrelAppApp.swift
+//  SecretSquirrelApp
+//
+//  Created by Jerrad Thramer on 4/14/22.
+//
+
+import SwiftUI
+
+@main
+struct SecretSquirrelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
