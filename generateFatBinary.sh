66309f5060826e42faaa5f0e087ce12bbe5aa65f
Jerrad Thramer <Jerrad.Thramer@gmail.com>
2022-04-14 16:20:27 -0600

    Fixing issue where debug script was clobbering release products at release build time.

--- generateFatBinary.sh
+++ generateFatBinary.sh
@@ -1,7 +1,7 @@
 !#/bin/sh
 
-rm -r build/*
-rm -rf Products/*.xcframework
+rm -rf build/* && \
+rm -rf Products/*.xcframework && \
 
 xcodebuild archive \
 -project SecretSquirrelFramework/SecretSquirrelFramework.xcodeproj \
@@ -10,7 +10,7 @@ xcodebuild archive \
 -destination 'generic/platform=iOS Simulator' \
 -archivePath './build/SecretSquirrelFramework-iphonesimulator.xcarchive' \
 SKIP_INSTALL=NO \
-BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
+BUILD_LIBRARIES_FOR_DISTRIBUTION=YES && \
 
 xcodebuild archive \
 -project SecretSquirrelFramework/SecretSquirrelFramework.xcodeproj \
@@ -19,11 +19,11 @@ xcodebuild archive \
 -destination 'generic/platform=iOS' \
 -archivePath './build/SecretSquirrelFramework-iphoneos.xcarchive' \
 SKIP_INSTALL=NO \
-BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
+BUILD_LIBRARIES_FOR_DISTRIBUTION=YES && \
 
 xcodebuild -create-xcframework \
--archive  'build/SecretSquirrelFramework-iphonesimulator.xcarchive' \
+-archive  './build/SecretSquirrelFramework-iphonesimulator.xcarchive' \
 -framework 'SecretSquirrelFramework.framework' \
--archive 'build/SecretSquirrelFramework-iphoneos.xcarchive' \
+-archive './build/SecretSquirrelFramework-iphoneos.xcarchive' \
 -framework 'SecretSquirrelFramework.framework' \
 -output 'Products/SecretSquirrel.xcframework'
